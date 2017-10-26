95a6d0ac48552754d06f5868332ec312433013c9
OhBeomSeok <daobs@hanmail.net>
2017-10-26 10:53:12 +0900

    1. 테스트 2. 3. 오범석

--- seedTest/src/main/java/com/spring/seed/controller/SeedController.java
+++ seedTest/src/main/java/com/spring/seed/controller/SeedController.java
@@ -1,11 +1,8 @@
 package com.spring.seed.controller;
 
-import java.util.List;
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
